acf49d82ca2f643b962dc991a55b24281222b612
Devepre <bicycle2life@gmail.com>
2017-10-15 19:11:37 +0300

    check for Phone.equals

--- JavaOOP/src/lection01/task04/Main.java
+++ JavaOOP/src/lection01/task04/Main.java
@@ -2,39 +2,41 @@ package lection01.task04;
 
 public class Main {
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		Network kyivCell = new Network("KyivCell");
 		kyivCell.setName("KyivCell 4G");
-		
+
 		Phone nokiaOne = new Phone("Nokia S", "097-999-66-55");
 		Phone samsungOne = new Phone();
 		Phone samsungTwo = new Phone("Samsung Galaxy", "094-666-44-78");
 		Phone samsungThree = new Phone("Lenovo", "094-666-44-78");
-		
-		nokiaOne.registerToNetwork(kyivCell);	
+
+		nokiaOne.registerToNetwork(kyivCell);
 		kyivCell.registerNewPhone(samsungOne);
 		samsungTwo.registerToNetwork(kyivCell);
 		samsungThree.registerToNetwork(kyivCell);
-		
+
 		samsungOne.setOn(true);
 		samsungOne.call("1111");
-				
+
 		nokiaOne.call("097-999-66-55");
 		nokiaOne.call("094-666-44-78");
-		
+
 		nokiaOne.setOn(true);
 		nokiaOne.call("097-999-66-55");
 		nokiaOne.call("094-666-44-78");
-		
+
 		samsungTwo.setOn(true);
 		samsungTwo.call("097-999-66-55");
 		nokiaOne.call("094-666-44-78");
 		nokiaOne.call("911");
-				
+
 		samsungTwo.setOn(false);
 		nokiaOne.setOn(false);
-		
-		//System.out.println(kyivCell);
+
+		nokiaOne.registerToNetwork(kyivCell);
+
+		// System.out.println(kyivCell);
 
 	}
 
