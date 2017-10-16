9bf6b501e8052904e696b9578f7cfbe9a1b9b4b2
Devepre <bicycle2life@gmail.com>
2017-10-17 00:24:34 +0300

    implementing Board class

---
+++ JavaOOP/src/lection02/task01/Board.java
@@ -0,0 +1,105 @@
+package lection02.task01;
+
+import java.util.Arrays;
+
+public class Board {
+	public static final int CAPACITY = 4;
+	private Shape[] storage;	
+
+	public Board() {
+		super();
+		storage = new Shape[CAPACITY];
+	}
+	
+	public void printBoard() {
+		double totalArea = 0;
+		for (int i = 0; i < storage.length; i++) {
+			if (storage[i] != null) {
+				totalArea += storage[i].getArea();
+			}
+		}
+		System.out.println(this);
+		System.out.println("Total area of " + storage.getClass().getEnclosingClass() + " are: " + totalArea);
+	}
+	
+	public boolean addShape(Shape shape) {
+		for (int i = 0; i < storage.length; i++) {
+			if (storage[i] == null) {
+				storage[i] = shape;
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public boolean addShape(int index, Shape shape) {
+		if (!checkIndex(index)) {
+			return false;
+		}
+		if (storage[index] != null) {
+			return false;
+		}
+		storage[index] = shape;
+		return true;
+	}
+	
+	public Shape get(int index) {
+		if (checkIndex(index)) {			
+			Shape shape = storage[index];
+			return shape;
+		}
+		return null;		
+	}
+	
+	public Shape set(int index, Shape shape) {
+		if (checkIndex(index)) {
+			storage[index] = shape;
+			return storage[index];
+		}
+		return null;
+	}
+	
+	public Shape remove(int index) {
+		if (checkIndex(index)) {
+			Shape shape = storage[index];
+			storage[index] = null;
+			return shape;
+		}
+		return null;
+	}
+	
+	private boolean checkIndex(int index) {
+		if (index >= storage.length || index < 0) {
+			return false;
+		}
+		return true;
+	}
+	
+	public void clear() {
+		storage = new Shape[CAPACITY];
+	}
+	
+	public int size() {
+		int size = 0;
+		for (int i = 0; i < storage.length; i++) {
+			if (storage[i] != null) {
+				size++;
+			}
+		}
+		return size;
+	}
+	
+	public int capacity() {
+		return CAPACITY;
+	}
+		
+	public Shape[] getStorage() {
+		return storage;
+	}
+
+	@Override
+	public String toString() {
+		return this.getClass().getSimpleName() + Arrays.toString(storage);
+	}
+
+}
