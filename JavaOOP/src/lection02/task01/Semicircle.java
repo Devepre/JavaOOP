1b4a3282681e715c8f3fc8637d76fa8dac3a5eec
Devepre <bicycle2life@gmail.com>
2017-10-17 00:24:59 +0300

    implementing Semicircle class

--- JavaOOP/src/lection02/task01/Semicircle.java
+++ JavaOOP/src/lection02/task01/Semicircle.java
@@ -2,6 +2,10 @@ package lection02.task01;
 
 public class Semicircle extends Circle {
 
+	public Semicircle(Point centerPoint, Point radiusPoint) {
+		super(centerPoint, radiusPoint);
+	}
+	
 	@Override
 	public double getPerimetr() {
 		if (checkNullPoints()) {
