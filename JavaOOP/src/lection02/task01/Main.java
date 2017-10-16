5b8d3bcde62ac177ba642e276156f6adc03ecf45
Devepre <bicycle2life@gmail.com>
2017-10-17 00:38:25 +0300

    Test Main functions

--- JavaOOP/src/lection02/task01/Main.java
+++ JavaOOP/src/lection02/task01/Main.java
@@ -3,12 +3,14 @@ package lection02.task01;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Work with separate objects:");
+		
 		Point pOne = new Point(1, 1);
 		Point pTwo = new Point(0, 2);
 		Point pThree = new Point(0, 3);
 		
 		Triangle tri = new Triangle(pOne, pTwo, pThree);
-		//Triangle tri = new Triangle(null, null, null);
+		Triangle triN = new Triangle(null, null, null);
 		double triArea = tri.getArea();
 		double triPerimetr = tri.getPerimetr();
 
@@ -20,6 +22,25 @@ public class Main {
 		System.out.println(cir);
 		System.out.println("Circle length is: " + cir.getPerimetr());
 		System.out.println("Circle area is: " + cir.getArea());
+		
+		Semicircle semiCircle = new Semicircle(new Point(), pTwo);
+		System.out.println("Semicircle length is; " + semiCircle.getPerimetr());
+		System.out.println("Semicircle area is: " + semiCircle.getArea());
+		
+		System.out.println();
+		System.out.println("Work with Board: ");
+		
+		Board shBoard = new Board();
+		shBoard.printBoard();
+		shBoard.addShape(tri);		
+		shBoard.addShape(2, cir);	
+		shBoard.printBoard();
+		shBoard.addShape(1, semiCircle);
+		//shBoard.addShape(triN);		
+		shBoard.addShape(new Rectangle(pThree, pOne, pTwo, new Point()));
+		
+		shBoard.printBoard();
+	
 	}
 
 }
